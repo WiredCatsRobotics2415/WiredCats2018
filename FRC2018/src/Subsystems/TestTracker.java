48ce16bd83d5df99e32253713823067603e977dc
yashkadadi <yashkadadi@10.40.22.0>
2018-02-17 16:27:49 -0500

    Finished first draft (with intense trig and stuff)

--- FRC2018/src/Subsystems/TestTracker.java
+++ FRC2018/src/Subsystems/TestTracker.java
@@ -9,18 +9,19 @@ public class TestTracker extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	double[] pos = {0, 0}; //x and y
-	double[] lastTick = {0, 0}; //left and right
-	double deltaLeftEn, deltaRightEn;
-	final double ROBOT_WIDTH = 26.0; //inches
-	//double lastTheta, deltaTheta;
+	private double[] pos = {0, 0}; //x and y
+	private double[] lastTick = {0, 0}; //left and right
+	private double deltaLeftEn, deltaRightEn;
+	private final double ROBOT_WIDTH = 26.0; //inches
+	private double centralAngle;
+	private double lastAngle = 0;
 	public void updatePos(double[] thisTick, double angle) {
 		//find change in left and right encoder values in inches
 		this.deltaLeftEn = thisTick[0]-lastTick[0];
 		this.deltaRightEn = thisTick[1]-lastTick[1];
 		this.lastTick[0] = thisTick[0];
 		this.lastTick[1] = thisTick[1];
-		double meanRadius = this.calcCircle();
+		calcNet(this.calcCircle(), angle);
 	}
 	private double calcCircle() {
 		double greaterArc, lesserArc; //lol that Halo reference only true Halo fans understand
@@ -34,10 +35,23 @@ public class TestTracker extends Subsystem {
 		 * 
 		 */
 		double lesserRadius = (lesserArc * ROBOT_WIDTH)/(greaterArc-lesserArc);
-		double centralAngle = 180*(greaterArc-lesserArc)/(Math.PI*ROBOT_WIDTH);
+		this.centralAngle = 180*(greaterArc-lesserArc)/(Math.PI*ROBOT_WIDTH);
 		double meanRadius = lesserRadius + (ROBOT_WIDTH/2);
 		double netDistance = Math.sqrt((2*Math.pow(meanRadius, 2))-(2*meanRadius*Math.cos(centralAngle)));
-		return 0.0;
+		return netDistance;
+	}
+	private void calcNet(double netDistance, double thisAngle) {
+		double vectorAngle = (180-this.centralAngle)/2;
+		if (this.deltaLeftEn < this.deltaRightEn) { //turning left
+			vectorAngle *= -1;
+		}
+		double netAngle = lastAngle + thisAngle;
+		this.pos[0] += Math.sin(Math.toRadians(netAngle))*netDistance;
+		this.pos[1] += Math.cos(Math.toRadians(netAngle))*netDistance;
+		lastAngle = thisAngle;
+	}
+	public double[] getPos() {
+		return this.pos;
 	}
 
     public void initDefaultCommand() {
