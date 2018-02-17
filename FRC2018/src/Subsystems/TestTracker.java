e582229de11ef31b7e0a91d5c8d2e590d90189a7
yashkadadi <yashkadadi@10.40.22.0>
2018-02-17 15:56:51 -0500

    Added stuff

--- FRC2018/src/Subsystems/TestTracker.java
+++ FRC2018/src/Subsystems/TestTracker.java
@@ -13,15 +13,33 @@ public class TestTracker extends Subsystem {
 	double[] lastTick = {0, 0}; //left and right
 	double deltaLeftEn, deltaRightEn;
 	final double ROBOT_WIDTH = 26.0; //inches
-	double deltaTheta;
+	//double lastTheta, deltaTheta;
 	public void updatePos(double[] thisTick, double angle) {
 		//find change in left and right encoder values in inches
 		this.deltaLeftEn = thisTick[0]-lastTick[0];
 		this.deltaRightEn = thisTick[1]-lastTick[1];
 		this.lastTick[0] = thisTick[0];
 		this.lastTick[1] = thisTick[1];
-		
+		double meanRadius = this.calcCircle();
 	}
+	private double calcCircle() {
+		double greaterArc, lesserArc; //lol that Halo reference only true Halo fans understand
+		greaterArc = Math.max(deltaLeftEn, deltaRightEn);
+		lesserArc = Math.min(deltaLeftEn, deltaRightEn);
+		/*
+		 * Solving:
+		 * if x = degrees in arc and r = arc of lesser radius
+		 * x/360 * 2 * pi * r = lesserArc
+		 * x/360 * 2 * pi * (r+robotWidth) = greaterArc
+		 * 
+		 */
+		double lesserRadius = (lesserArc * ROBOT_WIDTH)/(greaterArc-lesserArc);
+		double centralAngle = 180*(greaterArc-lesserArc)/(Math.PI*ROBOT_WIDTH);
+		double meanRadius = lesserRadius + (ROBOT_WIDTH/2);
+		double netDistance = Math.sqrt((2*Math.pow(meanRadius, 2))-(2*meanRadius*Math.cos(centralAngle)));
+		return 0.0;
+	}
+
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
